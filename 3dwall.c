1cdb0434fdafbe48dca5e185ccfdf35b7d12b9e0
Zakaria Bouhamidi <zakbouha@student.1337.ma>
2023-11-06 11:53:54 +0100

    fake3dWalls finished with ShadowGradeColor  -- FishBlowProblem -- fixed well

--- 3dwall.c
+++ 3dwall.c
@@ -1,26 +1,34 @@
 #include "minimap.h"
 
+static int    _trgb(int t, int r, int g, int b)
+{
+    return (t << 24 | r << 16 | g << 8 | b);
+}
+
+static int    grade_color(double it)
+{
+	return (_trgb(0, 255, 255 - (it * 0.35), 255 - (it * 0.15)));
+}
+
 void	_fake3d_wall(t_global *_g)
 {
 	t_rays	*ray;
 	double	wallheight;
 	double	raydistance;
 	double	dis_projectplane;
-	// loop every ray in the array of rays
+
 	for (int i = 0; _g->rays[i]; i++)
 	{
 		ray = _g->rays[i];
-		raydistance = ray->distance;
+		raydistance = ray->distance * cos(ray->angleVeiw - _g->player->rotationAngle);	// that fixed the fishi-blow-view
 
-		// Calculate the distance to the projection plane
-		dis_projectplane = (WIDTH / 2) / tan(FOV_ANGLE / 2);
-		
-		// project wall height
-		wallheight = (GRID_SIZE / raydistance) * dis_projectplane;
+		dis_projectplane = (WIDTH / 2) / tan(FOV_ANGLE / 2);							// Calculate the distance to the projection plane
+
+		wallheight = (GRID_SIZE / raydistance) * dis_projectplane;						// project wall height
 	
 		draw_rect(_g->mlx_s, i * WallStripWidth,
 				(HEIGHT / 2) - (wallheight / 2),
 				WallStripWidth,
-				wallheight, WHITE);
+				wallheight, grade_color(raydistance));
 	}
 }
